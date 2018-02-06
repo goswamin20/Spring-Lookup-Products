84d527b4284301f77c8a45eff64d7989b251ccde
goswamin@iu.edu <goswamin@indiana.edu>
2018-02-06 10:54:04 -0500

    "commit basic files"

---
+++ src/main/java/com/goswamin/lookup/LookupApplication.java
@@ -0,0 +1,12 @@
+package com.goswamin.lookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookupApplication.class, args);
+	}
+}
